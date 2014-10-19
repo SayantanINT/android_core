03d168b406de43131276d8bdb5d07b9ef6926709
DmitryDzz <dzakhovds@gmail.com>
2014-10-19 21:08:09 +0400

    #2 OrientationView is ready to receive values and send commands

--- RoboBoard/src/main/java/ru/robotmitya/roboboard/OrientationView.java
+++ RoboBoard/src/main/java/ru/robotmitya/roboboard/OrientationView.java
@@ -8,6 +8,8 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import org.jetbrains.annotations.NotNull;
 
 public class OrientationView extends View {
@@ -16,10 +18,16 @@ public class OrientationView extends View {
 
     private GestureDetector mGestureDetector;
 
-//    private float mX = 0;
-//    private float mY = 0;
-
+    private float mRadius;
     private float mPointerRadius;
+    private float mStrokeWidth;
+    private float mScale;
+
+    private float mX = 1f;
+    private float mY = 0f;
+
+    private Vector2 mViewCenter = new Vector2();
+    private Vector2 mPointerCenter = new Vector2();
 
     private Paint mPaintBackground;
     private Paint mPaintPointer;
@@ -27,54 +35,81 @@ public class OrientationView extends View {
     @SuppressWarnings("UnusedDeclaration")
     public OrientationView(Context context) {
         super(context);
-        initGestureDetector();
+        init();
     }
 
     @SuppressWarnings("UnusedDeclaration")
     public OrientationView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initGestureDetector();
+        init();
     }
 
     @SuppressWarnings("UnusedDeclaration")
     public OrientationView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initGestureDetector();
+        init();
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        mPointerRadius = (right - left) * POINTER_SCALE;
+        final float width = right - left + 1;
+        final float height = bottom - top + 1;
+        mRadius = width / 2;
+        mPointerRadius = width * POINTER_SCALE;
+        mStrokeWidth = width * POINTER_BORDER_SCALE;
+        mScale = mRadius - mPointerRadius - mStrokeWidth;
+        mViewCenter.set(width / 2f, height / 2f);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        final float width = getWidth();
-        final float height = getHeight();
+        canvas.drawCircle(mViewCenter.x, mViewCenter.y, mRadius, mPaintBackground);
 
-        canvas.drawCircle(width / 2f, height / 2f, width / 2f, mPaintBackground);
-
-        final float strokeWidth = width * POINTER_BORDER_SCALE;
-        mPaintPointer.setStrokeWidth(strokeWidth);
+        mPaintPointer.setStrokeWidth(mStrokeWidth);
         final int colorId = isEnabled() ? R.color.orientation_pointer_enabled_color : R.color.orientation_pointer_disabled_color;
         mPaintPointer.setColor(getContext().getResources().getColor(colorId));
 
-        float pointerCenterX = width / 2f + 0; //...
-        float pointerCenterY = height / 2f + 0; //...
-        canvas.drawCircle(pointerCenterX, pointerCenterY, mPointerRadius, mPaintPointer);
+        mPointerCenter.set(mX, mY);
+        transformSquareToCircle(mPointerCenter);
+        mPointerCenter.scl(mScale);
+        mPointerCenter.add(mViewCenter);
+
+        canvas.drawCircle(mPointerCenter.x, mPointerCenter.y, mPointerRadius, mPaintPointer);
     }
 
-    private void initGestureDetector() {
+    private void transformSquareToCircle(Vector2 p) {
+        final float x = p.x;
+        final float y = p.y;
+        final float r = (float) Math.sqrt(x * x + y * y);
+        if (r > MathUtils.FLOAT_ROUNDING_ERROR) {
+            p.x = Math.signum(x) * x * x / r;
+            p.y = Math.signum(y) * y * y / r;
+        } else {
+            p.x = 0f;
+            p.y = 0f;
+        }
+    }
+
+    private void init() {
+        setEnabled(false);
+
+        initPaint();
+        initGestureDetector();
+    }
+
+    private void initPaint() {
         mPaintBackground = new Paint();
         mPaintBackground.setStyle(Paint.Style.FILL);
         mPaintBackground.setColor(getContext().getResources().getColor(R.color.board_button_background_color));
 
         mPaintPointer = new Paint();
         mPaintPointer.setStyle(Paint.Style.STROKE);
+    }
 
+    private void initGestureDetector() {
         mGestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onDown(MotionEvent e) {
